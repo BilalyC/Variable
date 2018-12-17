e4c62a521c44ec483fe4c9d23a3367dced3629f6
BilalyC <bilaly.camara@outlook.com>
2018-12-17 14:42:58 +0100

    Exercice 2 Complet

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            x = 2;
+            y = 5;
+            z = 4;
+
+            Console.WriteLine("Les 3 valeurs sont respectivement " + x + ", " + y + " et " + z);
+
+            Console.WriteLine("Le produit entre la troisieme valeur et la somme des deux premieres valeurs est " + (z *(x + y)));
+        }
+    }
+}
